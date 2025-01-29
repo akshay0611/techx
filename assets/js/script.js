c5375903f3417499df30ac5920f06ed2fdc4270b
Akshay <akshay.allen26200@gmail.com>
2025-01-29 12:53:06 +0530

    Add success message to hero form

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,3 +40,27 @@ window.addEventListener("scroll", function () {
     backTopBtn.classList.remove("active");
   }
 });
+
+// Hero form variables
+const heroForm = document.querySelector("[data-hero-form]");
+const heroFormInput = document.querySelector("[data-hero-form-input]");
+const successMessage = document.getElementById("hero-success-message");
+
+// Listen for form submit
+heroForm.addEventListener("submit", function (e) {
+  e.preventDefault(); // Prevent the default form submission
+
+  // Check if the form is valid
+  if (heroForm.checkValidity()) {
+    // Show success message
+    successMessage.style.display = "block";
+
+    // Clear the form input field
+    heroFormInput.value = "";
+
+    // Hide the success message after 3 seconds
+    setTimeout(() => {
+      successMessage.style.display = "none";
+    }, 3000);
+  }
+});
